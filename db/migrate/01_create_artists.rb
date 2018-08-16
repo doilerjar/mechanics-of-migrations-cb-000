7e4722bfaf2cc8baf6332f14aea3d3967597d33a
Kevin K <doilerjar@outlook.com>
2018-08-16 09:58:28 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,9 +1,17 @@
 class CreateArtists < ActiveRecord::Migration
+  
+  # Code to execute when the migration is run or "do"
   def up 
     
   end 
   
+  # Code to execute when the migration is rolled back or "undo"
   def down 
+    
+  end 
+  
+  # Primary way to write migrations
+  def change
     
   end 
   
